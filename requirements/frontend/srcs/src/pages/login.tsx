a4c786e8bffd3bc7942d54bc82a4fd9ab0864beb
Alexander SABOURET <asaboure@student.42.fr>
2022-12-30 20:22:07 +0100

    added validate page

--- requirements/frontend/srcs/src/pages/login.tsx
+++ requirements/frontend/srcs/src/pages/login.tsx
@@ -3,8 +3,9 @@ import LoginButton from "../components/buttons/buttons";
 
 const client_id = process.env.REACT_APP_APP_ID
 const app_uri = process.env.REACT_APP_APP_URI
-const state = "pouet"
-const loginUrl = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${app_uri}&response_type=code&scope=public&state=${state}'`;
+const redirect_uri = app_uri + "/validate"
+const state = "pouet2"
+const loginUrl = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=public&state=${state}'`;
 
 function LoginPage() {
   return (
---
+++ requirements/frontend/srcs/src/pages/validate.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+function validatePage() {
+  return (
+    <div>
+      <h1>Please Log In</h1>
+      {/* <LoginButton url={loginUrl} /> */}
+    </div>
+  );
+}
+
+export default validatePage;
