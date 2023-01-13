20238243109c290b91b550545e6128fa9fc1c980
Alexander Sabouret <asaboure@student.42.fr>
2023-01-13 18:01:12 +0100

    changed mainPage layout and added temporary styles

--- requirements/frontend/srcs/src/components/buttons/buttons.tsx
+++ requirements/frontend/srcs/src/components/buttons/buttons.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import classes from "./buttons.module.css";
 
 function LoginButton(props: LoginButtonProps) {
     const { url } = props;
 
     return (
         <a href={url}>
-            <button>Log In</button>
+            <button className={classes.button1}>LOGIN WITH 42</button>
         </a>
     );
 }
--- requirements/frontend/srcs/src/pages/mainPage.tsx
+++ requirements/frontend/srcs/src/pages/mainPage.tsx
@@ -1,46 +1,31 @@
-import { useEffect, useState } from "react";
 import { useContext } from "react";
+import classes from "./mainPage.module.css";
 
-import UserList from "../components/Users/UserList";
+import LoginButton from "../components/buttons/buttons";
 import UserContext from "../store/user-context";
 
+const client_id = process.env.REACT_APP_APP_ID;
+const app_uri = process.env.REACT_APP_APP_URI;
+const redirect_uri = app_uri + "/validate";
+const state = "pouet2";
+const loginUrl = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=public&state=${state}'`;
+
 function MainPage() {
-  const [isLoading, setIsLoading] = useState(true);
-  const arr: any = [];
-  const [loadedUsers, setLoadedUsers] = useState(arr);
-  console.log("d");
   const userCtx: any = useContext(UserContext);
   if (userCtx.user.username) {
     console.log("logged in as " + userCtx.user.username);
   }
-  useEffect(() => {
-    fetch(`http://localhost:5000/users`)
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        const users = [];
-        for (const key in data) {
-          const user = {
-            id: key,
-            ...data[key],
-          };
-          users.push(user);
-        }
-        setIsLoading(false);
-        setLoadedUsers(users);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   return (
-    <div>
-      <UserList users={loadedUsers} />
+    <div className={classes.splash_middle}>
+      <img
+        src="https://cdn.discordapp.com/attachments/293910473663971328/1041748304377155704/kamaboko_a_favicon_for_a_website_where_you_can_play_pong_7e1346ef-31c0-47ec-b09a-4455365e1ef6.png"
+        className={classes.splash_logo}
+        alt=""
+      />
+      <div className={classes.splash_content}>
+        <LoginButton url={loginUrl} />
+      </div>
     </div>
   );
 }
