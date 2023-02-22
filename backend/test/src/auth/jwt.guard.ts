a242cb4f07bcfded6f50ea54db75c0e5a7f43e30
Guillaume CHOPIN <gchopin@e1r8p5.clusters.42paris.fr>
2023-02-22 14:09:03 +0100

    update passport

--- backend/test/src/auth/jwt.guard.ts
+++ backend/test/src/auth/jwt.guard.ts
@@ -4,6 +4,7 @@ import { ExtractJwt } from 'passport-jwt';
 //import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { Reflector } from '@nestjs/core';
+import { WsException } from '@nestjs/websockets';
 
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
@@ -24,19 +25,22 @@ export class JwtGuard extends AuthGuard('jwt') {
         console.log("enter guard jwt");
         if (isPublic)
             return (true);
-        if (typeof request.route != "undefined")
+        if (typeof request.route != "undefined"
+            && typeof request.headers.authorization != "undefined")
             bearer = request.headers.authorization.split('Bearer ')[1];
-        else if (typeof request.route == "undefined")
-        {
+        else if (typeof request.route == "undefined") {
             bearer = request.handshake.headers.authorization;
         }
         if (typeof bearer == "undefined")
             return (false);
         const decoded = this.authService.verifyToken(bearer);
-        if (decoded === false)
-                return (false);
-        if (typeof request.route == "undefined")
-        {
+        if (decoded === false && typeof request.route == "undefined") {
+            console.log("decoded false");
+            throw new WsException('Token not valid');;
+        }
+        else if (decoded === false)
+            return (false);
+        if (typeof request.route == "undefined") {
             console.log("bearer: " + bearer);
             request.headers = {};
             request.headers.authorization = "Bearer " + bearer;
