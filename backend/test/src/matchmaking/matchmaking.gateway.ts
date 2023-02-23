be5ca3a538b7bf6c70cb81768b197bacfaa58039
Daniel Atack <daniel.atack@hotmail.fr>
2023-02-23 17:06:50 +0100

    ws errors

--- backend/test/src/matchmaking/matchmaking.gateway.ts
+++ backend/test/src/matchmaking/matchmaking.gateway.ts
@@ -1,19 +1,38 @@
 import { JwtService } from '@nestjs/jwt';
 import {
-    SubscribeMessage, WebSocketGateway, MessageBody
-    , ConnectedSocket, WebSocketServer, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect, WsException
-  } from '@nestjs/websockets';
-  import { Socket, Server } from 'socket.io';
-import { Controller, Request, Req, Query, Param, Get, Post, Body, HttpException, HttpStatus, UseGuards, Logger, NotImplementedException } from '@nestjs/common';
+  SubscribeMessage,
+  WebSocketGateway,
+  MessageBody,
+  ConnectedSocket,
+  WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WsException,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+import {
+  Controller,
+  Request,
+  Req,
+  Query,
+  Param,
+  Get,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+  UseGuards,
+  Logger,
+  NotImplementedException,
+} from '@nestjs/common';
 import { JwtGuard } from 'src/auth/jwt.guard';
 import { User } from '../chat/chat.interface';
 
 const { FifoMatchmaker } = require('matchmaking');
 
-
-
 @WebSocketGateway({
-  namespace: 'matchmaking'
+  namespace: 'matchmaking',
 })
 export class MatchMakingGateway
   implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
@@ -21,28 +40,22 @@ export class MatchMakingGateway
   @WebSocketServer()
   server: Server;
 
-  constructor(
-    private readonly jwtService: JwtService,
-  ) {}
+  constructor(private readonly jwtService: JwtService) {}
 
-  afterInit(server: Server) { }
+  afterInit(server: Server) {}
 
   async handleConnection(client: Socket) {}
-  
+
   @UseGuards(JwtGuard)
   @SubscribeMessage('queuein')
-  async queuein(
-    @ConnectedSocket() socket: Readonly<any>,
-  ) {
+  async queuein(@ConnectedSocket() socket: Readonly<any>) {
     try {
-      console.log("queue in");
+      console.log('queue in');
       const user = socket.user;
 
-      if (typeof user.userID != "number")
-        return (false);
-      } 
-    catch (error) {
-      console.log("matchmaking failed");
+      if (typeof user.userID != 'number') return false;
+    } catch (error) {
+      console.log('matchmaking failed');
       this.server.to(error.response.recipient).emit('matchmakingfailed', {
         message: error.response.message,
         match: error.response.match,
@@ -51,67 +64,62 @@ export class MatchMakingGateway
     }
   }
 
+  @UseGuards(JwtGuard)
   @SubscribeMessage('queueout')
   queueout(@ConnectedSocket() client: Socket) {
     try {
-        // leave queue here?
-    
-          }
-         catch (error: any) { 
-            throw new HttpException({
-              status: HttpStatus.FORBIDDEN,
-              error: 'This is a custom message',
-            }, HttpStatus.FORBIDDEN, {
-              cause: error
-            });
-          }
+      // leave queue here?
+    } catch (error) {
+      console.log('leaving queue failed');
+      this.server.to(error.response.recipient).emit('queueoutfailed', {
+        message: error.response.message,
+        match: error.response.match,
+      });
+      throw new WsException(error);
+    }
   }
 
+  @UseGuards(JwtGuard)
   @SubscribeMessage('acceptMMmatch')
   async accept(@ConnectedSocket() client: Socket) {
     try {
-        // accepting matchmaking match after queue found a match
-    
-          }
-         catch (error: any) { 
-            throw new HttpException({
-              status: HttpStatus.FORBIDDEN,
-              error: 'This is a custom message',
-            }, HttpStatus.FORBIDDEN, {
-              cause: error
-            });
-          }
+      // accepting matchmaking match after queue found a match
+    } catch (error) {
+      console.log('accept match failed');
+      this.server.to(error.response.recipient).emit('acceptMMmatchfailed', {
+        message: error.response.message,
+        match: error.response.match,
+      });
+      throw new WsException(error);
+    }
   }
 
+  @UseGuards(JwtGuard)
   @SubscribeMessage('declineMMmatch')
   decline(@ConnectedSocket() client: Socket) {
     try {
-        // refusing matchmaking match after queue found a match
-    
-          }
-         catch (error: any) { 
-            throw new HttpException({
-              status: HttpStatus.FORBIDDEN,
-              error: 'This is a custom message',
-            }, HttpStatus.FORBIDDEN, {
-              cause: error
-            });
-          }
+      // refusing matchmaking match after queue found a match
+    } catch (error) {
+      console.log('decline match failed');
+      this.server.to(error.response.recipient).emit('declineMMmatchfailed', {
+        message: error.response.message,
+        match: error.response.match,
+      });
+      throw new WsException(error);
+    }
   }
 
-  handleDisconnect(client: Socket){
+  @UseGuards(JwtGuard)
+  handleDisconnect(client: Socket) {
     try {
-        // leave the page? = leave queue
-    
-          }
-         catch (error: any) { 
-            throw new HttpException({
-              status: HttpStatus.FORBIDDEN,
-              error: 'This is a custom message',
-            }, HttpStatus.FORBIDDEN, {
-              cause: error
-            });
-          }
+      // leave the page? = leave queue
+    } catch (error) {
+      console.log('disconnect MM failed');
+      this.server.to(error.response.recipient).emit('disconnectMMfailed', {
+        message: error.response.message,
+        match: error.response.match,
+      });
+      throw new WsException(error);
+    }
   }
-
 }
