dbdc513c8d04a2b894edb8d69c7f853e2daa025e
dclark <dclark@student.42.fr>
2023-02-27 10:48:12 +0100

    stat table added

--- backend/test/src/typeorm/user.entity.ts
+++ backend/test/src/typeorm/user.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, OneToMany, OneToOne, PrimaryColumn } from 'typeorm';
+import { BlackFriendList } from './blackFriendList.entity';
+import { Stat } from './stat.entity';
 
 @Entity()
 export class User {
@@ -31,4 +33,11 @@ export class User {
     default: '',
   })
   token: string;
+  //OneToMany vers blackfriendList
+  @OneToMany(() => BlackFriendList, (blackfriendlist) => blackfriendlist.owner_id)
+  lstBlackFriendOwner: BlackFriendList[];
+  @OneToMany(() => BlackFriendList, (blackfriendlist) => blackfriendlist.userFocus)
+  lstBlackFriendFocus: BlackFriendList[];
+  @OneToOne(() => Stat, (stat) => stat.user)
+  sstat: Stat[]
 }
