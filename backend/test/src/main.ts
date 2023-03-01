05caddd97ca000d2c010f10f2b1ab8fd5bcba0cd
dclark <dclark@student.42.fr>
2023-03-01 17:56:42 +0100

    upload avatar depuis page /profile

--- backend/test/src/main.ts
+++ backend/test/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'; 
 import * as cookieParser from 'cookie-parser';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.use(cookieParser());
+  app.use(json({limit: '10mb'}));
+  app.use(urlencoded({extended: true, limit: '10mb'}));
   await app.listen(3000);
 }
 bootstrap();
