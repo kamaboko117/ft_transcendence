39a98c082cbf572763e66445d82aed4728318c95
gchopin <gchopin@student.42.fr>
2023-04-10 11:16:24 +0200

    ajout npm audit fix, debug de mauvais emplacement de composant

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -5,10 +5,12 @@ if [ ! -d ./node_modules ];
 then
 	npm i -g npm@latest
 	npm i -g @nestjs/cli@latest
+	npm audit fix
 	npm install
 else
 	npm i -g npm@latest
 	npm i -g @nestjs/cli@latest
+	npm audit fix
 	npm update
 	npm update -g
 fi
