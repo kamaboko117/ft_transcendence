b2eff54a99b9272c3e015604fdb1a60597d6498b
Guillaume CHOPIN <gchopin@e1r8p5.clusters.42paris.fr>
2023-04-12 11:23:33 +0200

    test prod

--- backend/dist/auth/local.strategy.d.ts
+++
@@ -1,9 +0,0 @@
-import { Strategy } from 'passport-custom';
-import { AuthService } from './auth.service';
-declare const LocalStrategy_base: new (...args: any[]) => Strategy;
-export declare class LocalStrategy extends LocalStrategy_base {
-    private authService;
-    constructor(authService: AuthService);
-    validate(req: any): Promise<any>;
-}
-export {};
