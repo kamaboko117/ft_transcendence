344e174770ef597b046cdac1fb1482bf42a27c00
Guillaume CHOPIN <gchopin@e1r8p5.clusters.42paris.fr>
2023-04-12 11:23:33 +0200

    test prod

--- backend/dist/typeorm/blackFriendList.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user.entity';
-export declare class BlackFriendList {
-    id: number;
-    type_list: number;
-    userOwner: User;
-    owner_id: number;
-    userFocus: User;
-    focus_id: number;
-}
--- docker/frontend/entrypoint.sh
+++ docker/frontend/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-#if [ ! -d ./node_modules ];
-#then
-#	npm i -g npm@latest
-#	npm i -g @nestjs/cli@latest
-#	npm audit fix
-#	npm install
-#else
-#	npm i -g npm@latest
-#	npm i -g @nestjs/cli@latest
-#	npm audit fix
-#	npm update
-#	npm update -g
-#fi
+if [ ! -d ./node_modules ];
+then
+	npm i -g npm@latest
+	npm i -g @nestjs/cli@latest
+	npm audit fix
+	npm install
+else
+	npm i -g npm@latest
+	npm i -g @nestjs/cli@latest
+	npm audit fix
+	npm update
+	npm update -g
+fi
 exec "$@"
