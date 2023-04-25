c12fd552fe92143efe5e470ccc5980dc5bd07969
Alexander Sabouret <asaboure@student.42.fr>
2023-04-25 17:01:32 +0200

    added score to win, fixed format, removed useless stuff

--- backend/src/socket/socketEvents.ts
+++ backend/src/socket/socketEvents.ts
@@ -48,6 +48,7 @@ interface IGame {
   player1: IPlayer;
   player2: IPlayer;
   ball: IBall;
+  goal: number;
 }
 
 class Player implements IPlayer {
@@ -104,12 +105,18 @@ class Game implements IGame {
   player1: IPlayer;
   player2: IPlayer;
   ball: IBall;
-  constructor(id: string, player1id: string, player2id: string) {
+  goal: number;
+  constructor(id: string, player1id: string, player2id: string, goal: number) {
     this.id = id;
     this.type = "classic";
     this.player1 = new Player(0, CANVAS_HEIGHT / 2 - 100 / 2, player1id);
-    this.player2 = new Player(CANVAS_WIDTH - 10, CANVAS_HEIGHT / 2 - 100 / 2, player2id);
+    this.player2 = new Player(
+      CANVAS_WIDTH - 10,
+      CANVAS_HEIGHT / 2 - 100 / 2,
+      player2id
+    );
     this.ball = new Ball();
+    this.goal = goal;
   }
 }
 
@@ -132,7 +139,7 @@ function collision(player: IPlayer, ball: any) {
   );
 }
 
-function resetBall(ball : IBall) {
+function resetBall(ball: IBall) {
   ball.x = CANVAS_WIDTH / 2;
   ball.y = CANVAS_HEIGHT / 2;
   ball.speed = 5;
@@ -146,35 +153,11 @@ function resetBall(ball : IBall) {
   }, 1500);
 }
 
-function update(game: IGame) {
-  game.ball.x += game.ball.velocityX;
-  game.ball.y += game.ball.velocityY;
-  if (game.ball.y + game.ball.radius > CANVAS_HEIGHT || game.ball.y - game.ball.radius < 0) {
-    game.ball.velocityY = -game.ball.velocityY;
-  }
-  let player = game.ball.x < CANVAS_WIDTH / 2 ? game.player1 : game.player2;
-  if (collision(player, game.ball)) {
-    let collidePoint = game.ball.y - (player.y + player.height / 2);
-    collidePoint = collidePoint / (player.height / 2);
-    let angleRad = (Math.PI / 4) * collidePoint;
-    let direction = game.ball.x + game.ball.radius < CANVAS_WIDTH / 2 ? 1 : -1;
-    game.ball.velocityX = direction * game.ball.speed * Math.cos(angleRad);
-    game.ball.velocityY = game.ball.speed * Math.sin(angleRad);
-    game.ball.speed += 0.1;
-  }
-  if (game.ball.x - game.ball.radius < 0) {
-    game.player2.score++;
-    resetBall(game.ball);
-  } else if (game.ball.x + game.ball.radius > CANVAS_WIDTH) {
-    game.player1.score++;
-    resetBall(game.ball);
-  }
-}
-
 @WebSocketGateway({
   cors: {
-    origin: "http://127.0.0.1:4000", credential: true
-  }
+    origin: "http://127.0.0.1:4000",
+    credential: true,
+  },
 })
 export class SocketEvents {
   private readonly mapUserInGame: Map<string, number>;
@@ -182,7 +165,8 @@ export class SocketEvents {
     @Inject(forwardRef(() => UsersGateway))
     private readonly userGateway: UsersGateway,
     private readonly roomsService: RoomsService,
-    private readonly usersService: UsersService) {
+    private readonly usersService: UsersService
+  ) {
     this.mapUserInGame = new Map();
   }
   @WebSocketServer()
@@ -192,28 +176,74 @@ export class SocketEvents {
     console.log("Client connected: ", client.id);
   }
 
-  async endGame(game: IGame, winnerSocketId: string, loserSocketId: string) {
-    let winnerId = this.mapUserInGame.get(winnerSocketId);
-    let loserId = this.mapUserInGame.get(loserSocketId);
+  update(game: IGame) {
+    game.ball.x += game.ball.velocityX;
+    game.ball.y += game.ball.velocityY;
+    if (
+      game.ball.y + game.ball.radius > CANVAS_HEIGHT ||
+      game.ball.y - game.ball.radius < 0
+    ) {
+      game.ball.velocityY = -game.ball.velocityY;
+    }
+    let player = game.ball.x < CANVAS_WIDTH / 2 ? game.player1 : game.player2;
+    if (collision(player, game.ball)) {
+      let collidePoint = game.ball.y - (player.y + player.height / 2);
+      collidePoint = collidePoint / (player.height / 2);
+      let angleRad = (Math.PI / 4) * collidePoint;
+      let direction =
+        game.ball.x + game.ball.radius < CANVAS_WIDTH / 2 ? 1 : -1;
+      game.ball.velocityX = direction * game.ball.speed * Math.cos(angleRad);
+      game.ball.velocityY = game.ball.speed * Math.sin(angleRad);
+      game.ball.speed += 0.1;
+    }
+    if (game.ball.x - game.ball.radius < 0) {
+      game.player2.score++;
+      resetBall(game.ball);
+    } else if (game.ball.x + game.ball.radius > CANVAS_WIDTH) {
+      game.player1.score++;
+      resetBall(game.ball);
+    }
+    if (game.player1.score === game.goal) {
+      this.endGame(game, game.player1.socketId, game.player2.socketId);
+    } else if (game.player2.score === game.goal) {
+      this.endGame(game, game.player2.socketId, game.player1.socketId);
+    }
+  }
 
-    console.log(`winnerId: ${winnerId}, loserId: ${loserId}`)
+  async endGame(game: IGame, winnerSocketId: string, loserSocketId: string) {
+    const winnerId = this.mapUserInGame.get(winnerSocketId);
+    const loserId = this.mapUserInGame.get(loserSocketId);
+    let winner = "";
+    let loser = "";
+
+    console.log(`winnerId: ${winnerId}, loserId: ${loserId}`);
     if (winnerId && loserId) {
-      await this.usersService.updateHistory(game.type, winnerId, loserId, winnerId);
+      await this.usersService.findUsersById(winnerId).then((user) => {
+        if (user) winner = user.username;
+      });
+      await this.usersService.findUsersById(loserId).then((user) => {
+        if (user) loser = user.username;
+      });
+      await this.usersService.updateHistory(
+        game.type,
+        winnerId,
+        loserId,
+        winnerId
+      );
       await this.usersService.updateAchive(winnerId);
       await this.usersService.updateAchive(loserId);
     }
-    this.server.to(game.id).emit('end_game', { winnerId: winnerId, loserId: loserId });
+    this.server.to(game.id).emit("end_game", { winner: winner, loser: loser });
     clearInterval(game.intervalId);
-    games = games.filter(g => g.id !== game.id);
+    games = games.filter((g) => g.id !== game.id);
     this.mapUserInGame.delete(winnerSocketId);
     this.mapUserInGame.delete(loserSocketId);
     this.roomsService.deleteRoom(game.id);
-
   }
 
-  findGameByConnectedSocket(socketId: string) : null | IGame{
+  findGameByConnectedSocket(socketId: string): null | IGame {
     let game = null;
-    games.forEach(g => {
+    games.forEach((g) => {
       if (g.player1.socketId === socketId || g.player2.socketId === socketId) {
         game = g;
       }
@@ -239,10 +269,10 @@ export class SocketEvents {
 
     for (let [key, value] of map.entries()) {
       if (value === id) {
-        return (true);
+        return true;
       }
     }
-    return (false);
+    return false;
   }
 
   inviteUserToGame(userId: string, userIdFocus: string, idGame: string) {
@@ -250,8 +280,9 @@ export class SocketEvents {
 
     for (let [key, value] of map.entries()) {
       if (value === userIdFocus) {
-        this.server.to(key).emit('inviteGame',
-          { idGame: idGame, user_id: userId });
+        this.server
+          .to(key)
+          .emit("inviteGame", { idGame: idGame, user_id: userId });
       }
     }
   }
@@ -270,22 +301,20 @@ export class SocketEvents {
       client.leave(data.roomId);
       this.mapUserInGame.delete(client.id);
     }
-
   }
 
   /* search if user is in private room */
   checkIfUserFound(room: Room, clientId: string) {
     const map = this.userGateway.getMap();
-    const split = room?.roomName.split('|');
+    const split = room?.roomName.split("|");
     if (split) {
       for (let [key, value] of map.entries()) {
         if (key === clientId) {
-          if (value === split[0] || value === split[1])
-            return (true);
+          if (value === split[0] || value === split[1]) return true;
         }
       }
     }
-    return (false);
+    return false;
   }
   @UseGuards(JwtGuard)
   @SubscribeMessage("join_game")
@@ -293,17 +322,20 @@ export class SocketEvents {
     const user = client.user;
     //await client.join(data.roomId);
     console.log("New user joining room: ", data);
-    console.log(`user: ${user.userID} is joining room: ${data.roomId}`)
+    console.log(`user: ${user.userID} is joining room: ${data.roomId}`);
 
     const connectedSockets = this.server.sockets.adapter.rooms.get(data.roomId);
     const socketRooms = Array.from(client.rooms.values()).filter(
       (r) => r !== client.id
     );
     const room = await this.roomsService.findRoomById(data.roomId);
-    //j'enleve ca car le find fonctionne mal, il cherche sur toutes les rooms chat compris, 
+    //j'enleve ca car le find fonctionne mal, il cherche sur toutes les rooms chat compris,
     //si c pour trouver si deja en partie faut modifier
-    if (/*socketRooms.length > 0
-      || */(connectedSockets && connectedSockets?.size > 1)) {
+    if (
+      /*socketRooms.length > 0
+      || */ connectedSockets &&
+      connectedSockets?.size > 1
+    ) {
       client.emit("join_game_error", { error: "Room is full" });
       return;
     } else {
@@ -311,8 +343,8 @@ export class SocketEvents {
       this.mapUserInGame.set(client.id, user.userID);
       if (room && room.private === true) {
         const result: boolean = this.checkIfUserFound(room, client.id);
-        console.log("result: " + result)
-        console.log(this.server.sockets.adapter.rooms.get(data.roomId))
+        console.log("result: " + result);
+        console.log(this.server.sockets.adapter.rooms.get(data.roomId));
         if (result === false) {
           client.emit("join_game_error", { error: "You are spectactor" });
           return;
@@ -321,7 +353,7 @@ export class SocketEvents {
       client.emit("join_game_success", { roomId: data.roomId });
       if (connectedSockets?.size === 2) {
         let socket2 = connectedSockets?.values().next().value;
-        let newGame = new Game(data.roomId, client.id, socket2);
+        let newGame = new Game(data.roomId, client.id, socket2, 11);
         let player1 = newGame.player1;
         let player2 = newGame.player2;
         let ball = newGame.ball;
@@ -330,7 +362,7 @@ export class SocketEvents {
         client.emit("start_game", { side: 1 });
         client.to(data.roomId).emit("start_game", { side: 2 });
         newGame.intervalId = setInterval(() => {
-          update(newGame);
+          this.update(newGame);
           client.emit("on_game_update", {
             player1,
             player2,
@@ -346,11 +378,11 @@ export class SocketEvents {
     }
   }
 
-  @SubscribeMessage("update_game")
-  async update(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
-    const gameRoom: any = this.getSocketGameRoom(client);
-    client.to(gameRoom).emit("on_game_update", data);
-  }
+  // @SubscribeMessage("update_game")
+  // async update(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+  //   const gameRoom: any = this.getSocketGameRoom(client);
+  //   client.to(gameRoom).emit("on_game_update", data);
+  // }
 
   @SubscribeMessage("update_player_position")
   async updatePlayerPosition(
@@ -374,6 +406,6 @@ export class SocketEvents {
   }
 
   getMap() {
-    return (this.mapUserInGame);
+    return this.mapUserInGame;
   }
 }
--- frontend/src/components/rooms/RoomItem.tsx
+++ frontend/src/components/rooms/RoomItem.tsx
@@ -3,7 +3,6 @@ import React from "react";
 type RoomProps = {
   uid: string;
   roomName: string;
-  roomCapacity: number;
   join: any;
 };
 
@@ -15,7 +14,6 @@ function RoomItem(props: RoomProps) {
   return (
     <div className="room_item" onClick={joinRoom}>
       <h3 className="room_name">{props.roomName}</h3>
-      <h3>capacity: {props.roomCapacity}</h3>
     </div>
   );
 }
