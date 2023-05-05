1458a3983d1598cb8fbb6a4682770e9eaab550b1
Daniel Atack <daniel.atack@hotmail.fr>
2023-05-05 18:28:03 +0200

    fixed navbar

--- frontend/src/components/navbar/NavBar.tsx
+++ frontend/src/components/navbar/NavBar.tsx
@@ -48,7 +48,7 @@ export default function NavBar(props: {
 	)
 }
 
-function NavBarLink( to : any, children : any, ...props: any[] ) {
+function NavBarLink({ to, children, ...props} : any) {
 	const resolvedPath = useResolvedPath(to);
 	const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
